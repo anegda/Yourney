70f3a9ca5c59b255995cf83dc454c084af49b373
anegda <anegdaldama@gmail.com>
2023-05-08 15:02:57 +0200

    AÑADIR FOTO A GALERÍA

--- app/src/main/java/com/example/yourney/GaleriaFotosRuta.java
+++ app/src/main/java/com/example/yourney/GaleriaFotosRuta.java
@@ -13,9 +13,11 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
@@ -50,6 +52,7 @@ public class GaleriaFotosRuta extends AppCompatActivity {
         imagesRV = findViewById(R.id.imgRV);
         prepareRecyclerView();
 
+        //BOTÓN DE DESCARGAS
         Button descargar = (Button) findViewById(R.id.btn_descargar_todas);
         descargar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,6 +89,29 @@ public class GaleriaFotosRuta extends AppCompatActivity {
                 }
             }
         });
+
+        //BOTÓN DE ANADIR IMAGEN
+        Button btn_anadir = (Button) findViewById(R.id.btn_anadir_img);
+        btn_anadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ABRIMOS EL INTENT DE CAMARA Y GALERÍA
+                try{
+                    Intent i1 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                    Intent i2 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    Intent chooser = new Intent(Intent.ACTION_CHOOSER);
+                    chooser.putExtra(Intent.EXTRA_INTENT, i1);
+
+                    Intent[] intentArray = { i2 };
+                    chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                    startActivityForResult(chooser, 1); //ESTA DEPRECATED PERO FUNCIONA
+                }catch (Exception e){
+                    Log.d("DAS","Error la imagen no se carga correctamente");
+                }
+            }
+        });
     }
 
     private void prepareRecyclerView() {
@@ -106,5 +132,49 @@ public class GaleriaFotosRuta extends AppCompatActivity {
         imageBlobs.add(foto);
         imageBlobs.add(foto);
         imageRVAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            try {
+                Uri imageUri = data.getData();
+                if(imageUri==null){
+                    Bitmap img = (Bitmap) data.getExtras().get("data");
+
+                    //AÑADIMOS AL ARRAY Y NOTIFICAMOS
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    img.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                    byte[] b = baos.toByteArray();
+                    String fotoNueva = Base64.getEncoder().encodeToString(b);
+
+                    //TODO: LLAMADA A LA BD PARA SUBIR LA IMAGEN
+
+                    imageBlobs.add(fotoNueva);
+                    imageRVAdapter.notifyDataSetChanged();
+                }else {
+                    InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                    Bitmap img = BitmapFactory.decodeStream(imageStream);
+
+                    //AÑADIMOS AL ARRAY Y NOTIFICAMOS
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    img.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                    byte[] b = baos.toByteArray();
+                    String fotoNueva = Base64.getEncoder().encodeToString(b);
+
+                    //TODO: LLAMADA A LA BD PARA SUBIR LA IMAGEN
+
+                    imageBlobs.add(fotoNueva);
+                    imageRVAdapter.notifyDataSetChanged();
+
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(GaleriaFotosRuta.this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(GaleriaFotosRuta.this, "ERROR",Toast.LENGTH_SHORT).show();
+        }
     }
 }
