3074da914767f301f8aca2064e8040958c47faec
anegda <anegdaldama@gmail.com>
2023-05-22 17:34:57 +0200

    ENVIAR SOLICITUD COMPRUEBA ESTADO DE LOS CAMPOS

--- app/src/main/java/com/example/yourney/EnviarSolicitud.java
+++ app/src/main/java/com/example/yourney/EnviarSolicitud.java
@@ -9,6 +9,7 @@ import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -54,118 +55,128 @@ public class EnviarSolicitud extends AppCompatActivity {
         btn_enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Cogemos del campo el nombre de usuario
+                // Cogemos del campo el nombre de usuario / mensaje / usuario actual
                 EditText userEdit = findViewById(R.id.usuarioEdit);
                 String userPeti = userEdit.getText().toString();
+                EditText mensajeEdit = findViewById(R.id.mensajeEdit);
+                String mensaje = mensajeEdit.getText().toString();
+                Sesion sesion = new Sesion(EnviarSolicitud.this);
+                String username = sesion.getUsername();
 
-                // Comprobar credenciales contra la BD
-                Data datos = new Data.Builder()
-                        .putString("accion", "selectUsuario")
-                        .putString("consulta", "Usuarios")
-                        .putString("username", userPeti)
-                        .build();
+                // Comprobar que los campos no estén vacíos
+                if (mensaje.equals("") || userPeti.equals("")) {
+                    Toast.makeText(EnviarSolicitud.this, R.string.campos_vacios, Toast.LENGTH_LONG).show();
+                }
+                else if(userPeti.equals(username)){
+                    Toast.makeText(EnviarSolicitud.this, R.string.mensaje_error_peti, Toast.LENGTH_LONG).show();
+                }
+                else{
+                    // Comprobar credenciales contra la BD
+                    Data datos = new Data.Builder()
+                            .putString("accion", "selectUsuario")
+                            .putString("consulta", "Usuarios")
+                            .putString("username", userPeti)
+                            .build();
 
-                // Peticion al Worker
-                OneTimeWorkRequest select = new OneTimeWorkRequest.Builder(ConexionBD.class)
-                        .setInputData(datos)
-                        .build();
+                    // Peticion al Worker
+                    OneTimeWorkRequest select = new OneTimeWorkRequest.Builder(ConexionBD.class)
+                            .setInputData(datos)
+                            .build();
 
-                WorkManager.getInstance(EnviarSolicitud.this).getWorkInfoByIdLiveData(select.getId()).observe(EnviarSolicitud.this, new Observer<WorkInfo>() {
-                    @Override
-                    public void onChanged(WorkInfo workInfo) {
-                        // Gestiono la respuesta de la peticion
-                        if (workInfo != null && workInfo.getState().isFinished()) {
-                            Data output = workInfo.getOutputData();
-                            if (!output.getString("resultado").equals("Sin resultado")) {
-                                //si el usuario existe hacemos el insert
-                                // OBTENGO EL USUARIO ACTUAL y EL MENSAJE
-                                Sesion sesion = new Sesion(EnviarSolicitud.this);
-                                String username = sesion.getUsername();
-                                EditText mensajeEdit = findViewById(R.id.mensajeEdit);
-                                String mensaje = mensajeEdit.getText().toString();
+                    WorkManager.getInstance(EnviarSolicitud.this).getWorkInfoByIdLiveData(select.getId()).observe(EnviarSolicitud.this, new Observer<WorkInfo>() {
+                        @Override
+                        public void onChanged(WorkInfo workInfo) {
+                            // Gestiono la respuesta de la peticion
+                            if (workInfo != null && workInfo.getState().isFinished()) {
+                                Data output = workInfo.getOutputData();
+                                if (!output.getString("resultado").equals("Sin resultado")) {
+                                    //si el usuario existe hacemos el insert
 
-                                Data datosInsert = new Data.Builder()
-                                        .putString("accion", "insert")
-                                        .putString("consulta", "Peticiones")
-                                        .putString("username1", username)
-                                        .putString("username2", userPeti)
-                                        .putString("mensaje",mensaje)
-                                        .putInt("estado",0)
-                                        .build();
+                                    Data datosInsert = new Data.Builder()
+                                            .putString("accion", "insert")
+                                            .putString("consulta", "Peticiones")
+                                            .putString("username1", username)
+                                            .putString("username2", userPeti)
+                                            .putString("mensaje", mensaje)
+                                            .putInt("estado", 0)
+                                            .build();
 
-                                OneTimeWorkRequest insert = new OneTimeWorkRequest.Builder(ConexionBD.class)
-                                        .setInputData(datosInsert)
-                                        .build();
+                                    OneTimeWorkRequest insert = new OneTimeWorkRequest.Builder(ConexionBD.class)
+                                            .setInputData(datosInsert)
+                                            .build();
 
-                                WorkManager.getInstance(EnviarSolicitud.this).getWorkInfoByIdLiveData(insert.getId()).observe(EnviarSolicitud.this, new Observer<WorkInfo>() {
-                                    @Override
-                                    public void onChanged(WorkInfo workInfo) {
-                                        Toast.makeText(EnviarSolicitud.this, "OK", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                                WorkManager.getInstance(EnviarSolicitud.this).enqueue(insert);
+                                    WorkManager.getInstance(EnviarSolicitud.this).getWorkInfoByIdLiveData(insert.getId()).observe(EnviarSolicitud.this, new Observer<WorkInfo>() {
+                                        @Override
+                                        public void onChanged(WorkInfo workInfo) {
+                                            Toast.makeText(EnviarSolicitud.this, "OK", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                    WorkManager.getInstance(EnviarSolicitud.this).enqueue(insert);
 
-                                //mientras también cogemos el token del receptor
-                                Data datosSelectToken = new Data.Builder()
-                                        .putString("accion", "select")
-                                        .putString("consulta", "Tokens")
-                                        .putString("username", userPeti)
-                                        .build();
+                                    //mientras también cogemos el token del receptor
+                                    Data datosSelectToken = new Data.Builder()
+                                            .putString("accion", "select")
+                                            .putString("consulta", "Tokens")
+                                            .putString("username", userPeti)
+                                            .build();
 
-                                OneTimeWorkRequest selectToken = new OneTimeWorkRequest.Builder(ConexionBD.class)
-                                        .setInputData(datosSelectToken)
-                                        .build();
-                                WorkManager.getInstance(EnviarSolicitud.this).getWorkInfoByIdLiveData(selectToken.getId()).observe(EnviarSolicitud.this, new Observer<WorkInfo>() {
-                                    @Override
-                                    public void onChanged(WorkInfo workInfo) {
-                                        if (workInfo != null && workInfo.getState().isFinished()) {
-                                            Data output = workInfo.getOutputData();
-                                            if (!output.getString("resultado").equals("Sin resultado")) {
-                                                //si el usuario está logeado en algún dispositivo
-                                                JSONParser parser = new JSONParser();
-                                                try {
-                                                    JSONArray jsonResultado = (JSONArray) parser.parse(output.getString("resultado"));
+                                    OneTimeWorkRequest selectToken = new OneTimeWorkRequest.Builder(ConexionBD.class)
+                                            .setInputData(datosSelectToken)
+                                            .build();
+                                    WorkManager.getInstance(EnviarSolicitud.this).getWorkInfoByIdLiveData(selectToken.getId()).observe(EnviarSolicitud.this, new Observer<WorkInfo>() {
+                                        @Override
+                                        public void onChanged(WorkInfo workInfo) {
+                                            if (workInfo != null && workInfo.getState().isFinished()) {
+                                                Data output = workInfo.getOutputData();
+                                                if (!output.getString("resultado").equals("Sin resultado")) {
+                                                    //si el usuario está logeado en algún dispositivo
+                                                    JSONParser parser = new JSONParser();
+                                                    try {
+                                                        JSONArray jsonResultado = (JSONArray) parser.parse(output.getString("resultado"));
 
-                                                    Integer i = 0;
-                                                    System.out.println("***** " + jsonResultado + " *****");
-                                                    while (i < jsonResultado.size()) {
-                                                        JSONObject row = (JSONObject) jsonResultado.get(i);
-                                                        System.out.println("***** " + row + " *****");
-                                                        String Token = (String) row.get("Token");
+                                                        Integer i = 0;
+                                                        System.out.println("***** " + jsonResultado + " *****");
+                                                        while (i < jsonResultado.size()) {
+                                                            JSONObject row = (JSONObject) jsonResultado.get(i);
+                                                            System.out.println("***** " + row + " *****");
+                                                            String Token = (String) row.get("Token");
 
-                                                        Data datosNoti = new Data.Builder()
-                                                                .putString("accion", "notifPeticion")
-                                                                .putString("emisor", username)
-                                                                .putString("receptor", Token)
-                                                                .putString("mensaje", mensaje)
-                                                                .build();
-                                                        OneTimeWorkRequest notif = new OneTimeWorkRequest.Builder(ConexionBD.class)
-                                                                .setInputData(datosNoti)
-                                                                .build();
-                                                        WorkManager.getInstance(EnviarSolicitud.this).getWorkInfoByIdLiveData(notif.getId()).observe(EnviarSolicitud.this, new Observer<WorkInfo>() {
-                                                            @Override
-                                                            public void onChanged(WorkInfo workInfo) {
-                                                            }
-                                                        });
-                                                        WorkManager.getInstance(EnviarSolicitud.this).enqueue(notif);
-                                                        i=i+1;
+                                                            Data datosNoti = new Data.Builder()
+                                                                    .putString("accion", "notifPeticion")
+                                                                    .putString("emisor", username)
+                                                                    .putString("receptor", Token)
+                                                                    .putString("mensaje", mensaje)
+                                                                    .build();
+                                                            OneTimeWorkRequest notif = new OneTimeWorkRequest.Builder(ConexionBD.class)
+                                                                    .setInputData(datosNoti)
+                                                                    .build();
+                                                            WorkManager.getInstance(EnviarSolicitud.this).getWorkInfoByIdLiveData(notif.getId()).observe(EnviarSolicitud.this, new Observer<WorkInfo>() {
+                                                                @Override
+                                                                public void onChanged(WorkInfo workInfo) {
+                                                                    startActivity(new Intent(EnviarSolicitud.this, SolicitudesRecibidas.class));
+                                                                    finish();
+                                                                }
+                                                            });
+                                                            WorkManager.getInstance(EnviarSolicitud.this).enqueue(notif);
+                                                            i = i + 1;
+                                                        }
+                                                    } catch (ParseException e) {
+                                                        throw new RuntimeException(e);
                                                     }
-                                                }catch (ParseException e) {
-                                                    throw new RuntimeException(e);
                                                 }
                                             }
                                         }
-                                    }
-                                });
-                                WorkManager.getInstance(EnviarSolicitud.this).enqueue(selectToken);
+                                    });
+                                    WorkManager.getInstance(EnviarSolicitud.this).enqueue(selectToken);
 
-                            } else {
-                                Toast.makeText(EnviarSolicitud.this, R.string.peticion_incorrecta, Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(EnviarSolicitud.this, R.string.peticion_incorrecta, Toast.LENGTH_SHORT).show();
+                                }
                             }
                         }
-                    }
-                });
-                WorkManager.getInstance(EnviarSolicitud.this).enqueue(select);
+                    });
+                    WorkManager.getInstance(EnviarSolicitud.this).enqueue(select);
+                }
             }
         });
     }
